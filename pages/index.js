de9a8d8dc2fc237997b0268ab630b913acff5dc6
Orji Joseph <orjiace@gmail.com>
2021-02-07 22:31:02 +0100

    we added some cool page transition animation using framer

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,38 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 import Link from 'next/link'
+import {TimelineLite, Power3} from 'gsap'
+import React,{useEffect} from 'react'
+import {motion} from "framer-motion"
+
+
+//this is used together with framer motion for page transition animation
+const pageTransition = {
+    in: {
+        opacity: 1,
+        y: 0
+    },
+    out: {
+        opacity: 0,
+        y: "-10vh",
+    }
+}
+
 
 export default function Home() {
+    let t1 = new TimelineLite();
+
+    useEffect(() =>{
+        t1.from('.welcomeTxt', {
+            opacity: 0,
+            y: -10,
+            ease: Power3.easeInOut,
+        }).from('.subText', {
+            opacity: 0,
+            y: -10,
+            ease: Power3.easeInOut,
+        })
+    },[])
     return (
         <div className={styles.container}>
             <Head>
@@ -10,9 +40,9 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
-            <main className={styles.main}>
-                <h2>Hey buddy you're welcome</h2>
-                <div>
+            <motion.div exit="out" initial="out" animate="in" variants={pageTransition} className={styles.main}>
+                <h2 className='welcomeTxt'>Hey buddy you're welcome</h2>
+                <div className='subText'>
 
                     <small>This is just a quick, easy and simple formik and NextJs collaboration</small>
                 </div>
@@ -24,7 +54,7 @@ export default function Home() {
 
                 </Link>
 
-            </main>
+            </motion.div>
 
 
         </div>
--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React,{useEffect} from 'react'
 import {useFormik, Formik} from 'formik';
 import * as Yup from 'yup';
 import SignupStyles from '../../styles/Login.module.scss'
-
+import {TimelineLite, Power3, gsap} from 'gsap'
 import LoginForm from "../../component/LoginForm";
 import LoginSchema from "../../component/LoginSchema";
-import {FaEnvelope, FaKey, FaPaperPlane, FaUser} from "react-icons/fa";
-import MyInput from "../../component/Input";
+import {motion} from "framer-motion"
+import {useToast} from '@chakra-ui/react'
+
+const pageTransition = {
+    in: {
+        opacity: 1,
+        y: 0
+    },
+    out: {
+        opacity: 0,
+        y: "-10vh",
+    }
+}
+
 
 const SignupForm = () => {
+    let t1 = new TimelineLite();
+    const toast = useToast();
     const formik = useFormik({
         initialValues: {
             firstName: '',
@@ -27,36 +41,62 @@ const SignupForm = () => {
             password: Yup.string().min(3, "Passowrd Too short")
         }),
         onSubmit: (values) => {
-            alert(JSON.stringify(values, null, 2));
+           // alert(JSON.stringify(values, null, 2));
 
 
 
         },
     });
 
+    useEffect(() =>{
+        t1.from('.title', {
+            opacity: 0,
+            y: -10,
+            ease: Power3.easeIn,
+        }).from('.titleSmall', {
+            opacity: 0,
+            Y: 10,
+            ease: Power3.easeInOut,
+        })
+    },[])
     const handleSubmit = (values) =>{
-        alert(JSON.stringify(values, null, 2));
+       // alert(JSON.stringify(values, null, 2));
+        //to get your input values
+        //you can destructure them like so
 
+        const {firstName, password, lastName, email} = values
+        //do whatever you want with them
+
+        //this is simple toast message that pops up if all inputs fields are correctly filed
+        toast({
+            title: "Good boy!",
+            description: "You're in!",
+            duration: 6000,
+            position: "top",
+            status: "success",
+            isClosable: true,
+        })
     }
     return (
         <div className={SignupStyles.signupWrap}>
             <div className={SignupStyles.title}>
-                <span>
+                <span className='title'>
                     SIGNUP
                 </span>
             </div>
             <div className={SignupStyles.title}>
 
-                <small>
+                <small className='titleSmall'>
                     Chief, This is where the magic happens!
                 </small>
             </div>
-            
+
+            <motion.div exit="out" initial="out" animate="in" variants={pageTransition}>
             <Formik initialValues={{firstName: '',
                 lastName: '',
                 email: '',
                 password: '' }} validationSchema={LoginSchema} component={LoginForm} onSubmit={handleSubmit}/>
-
+        </motion.div>
         </div>
     );
 };
